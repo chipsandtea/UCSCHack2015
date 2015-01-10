4b06e09ae975bc4cef9acd6bc6cd4a638adffd3f
Brad Bernard <bradbernard@me.com>
2015-01-09 21:17:11 -0800

    PHP output schools

---
+++ applications.php
@@ -0,0 +1,24 @@
+<?php
+
+//require 'wp-include.php';
+global $wpdb;
+
+$data = array();
+
+$result = $wpdb->get_results(
+	"
+	SELECT post_title, ID
+	FROM $wpdb->posts
+	WHERE post_type = 'registration55'
+	"
+);
+
+foreach ($result as $application)
+{
+    $data['school'] = $application->post_title;
+    $data['id'] = $application->ID;
+}
+
+echo json_encode($data);
+
+?>
