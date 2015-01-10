bb6e329cc895320188fc1527a04f9d5a95661c0a
Brad Bernard <bradbernard@me.com>
2015-01-10 13:11:37 -0800

    PHP parse testing

--- parse.php
+++ parse.php
@@ -4,15 +4,39 @@ require 'wp-blog-header.php';
 
 global $wpdb;
 
-$json_data = json_decode($_POST['data']);
+//$json_data = json_decode($_POST['data']);
+$json_data = array(
+    'data' => array(
+        'group_data' => null
+    ),
+    'id' => 3482
+);
 
-$post_id = $wpdb->get_var("SELECT id FROM $wpdb->posts, $wpdb->postmeta WHERE wp_postmeta.post_id = wp_posts.ID");
 
-foreach($json_data as $field)
+
+if(have_rows('group_data', $json_data['id']))
 {
-    update_post_meta($post_id, $field['meta_key'], $field['meta_value']);
+    while (have_rows('group_data')) : the_row();
+
+        // Your loop code
+        echo the_sub_field('group_name');
+
+    endwhile;
+
+} else {
+
+    die(json_encode(['success' => false, 'message' => 'Invalid school ID.']));
+
 }
 
-echo json_encode(['success' => true, 'message' => 'Insert successful.']);
+//$post_id = $wpdb->get_var("SELECT id FROM wp_posts, wp_postmeta WHERE wp_postmeta.post_id = wp_posts.ID AND wp_posts.ID = " . $_POST['data']['id']);
+//
+//
+//foreach($json_data as $field)
+//{
+//    update_post_meta($post_id, $field['meta_key'], $field['meta_value']);
+//}
+
+//echo json_encode(['success' => true, 'message' => 'Update successful.']);
 
 ?>
