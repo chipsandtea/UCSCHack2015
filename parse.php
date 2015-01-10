51530a2702a6e872ec964fe66e0cf2d3f0160460
Brad Bernard <bradbernard@me.com>
2015-01-09 20:37:51 -0800

    PHP Side mock

---
+++ parse.php
@@ -0,0 +1,15 @@
+<?php
+
+//require 'wp-include.php';
+global $wpdb;
+
+$json_data = json_decode($_POST['data']);
+
+$post_id = $wpdb->get_var("SELECT id FROM $wpdb->posts, $wpdb->postmeta WHERE wp_postmeta.post_id = wp_posts.ID");
+
+foreach($json_data as $field)
+{
+    update_post_meta($post_id, $field['meta_key'], $field['meta_value']);
+}
+
+?>
