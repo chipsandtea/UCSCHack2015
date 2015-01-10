7d90536ae09ab284b4f599ca4d9389d1d37a1fb2
Brad Bernard <bradbernard@me.com>
2015-01-10 12:52:33 -0800

    Php Stuff rd 2

--- applications.php
+++ applications.php
@@ -8,15 +8,14 @@ $data = array();
 $result = $wpdb->get_results(
 	"
 	SELECT post_title, ID
-	FROM $wpdb->posts
-	WHERE post_type = 'registration55'
+	FROM wp_posts
+	WHERE post_type LIKE 'registration55'
 	"
 );
 
 foreach ($result as $application)
 {
-    $data['school'] = $application->post_title;
-    $data['id'] = $application->ID;
+    $data[] = ['school' => $application->post_title, 'id' => $application->ID];
 }
 
 echo json_encode($data);
