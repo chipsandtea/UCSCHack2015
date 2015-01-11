0733099cedd3f59a804361e9e496fc45da6c99f4
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2015-01-11 11:21:06 -0800

    Fixed Conflicts

--- HackUCSC2015/HackUCSC2015/HackData.m
+++ HackUCSC2015/HackUCSC2015/HackData.m
@@ -9,10 +9,26 @@
 #import "HackData.h"
 
 NSMutableDictionary* gHackData;         // singleton instance
+NSMutableDictionary* bHackData;         // singleton instance
+NSMutableDictionary* cHackData;         // singleton instance
 
 NSMutableDictionary* sharedData() {
     if (!gHackData) {
         gHackData = [NSMutableDictionary dictionaryWithCapacity:1];
     }
     return gHackData;
+}
+
+NSMutableDictionary* bData() {
+    if (!bHackData) {
+        bHackData = [NSMutableDictionary dictionaryWithCapacity:1];
+    }
+    return bHackData;
+}
+
+NSMutableDictionary* cData() {
+    if (!cHackData) {
+        cHackData = [NSMutableDictionary dictionaryWithCapacity:1];
+    }
+    return cHackData;
 }
