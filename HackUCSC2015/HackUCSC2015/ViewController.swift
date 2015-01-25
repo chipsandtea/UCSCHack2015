016771ccccdce51448e8eb84af8ef8fe815be625
Brad Bernard <bradbernard@me.com>
2015-01-24 17:10:09 -0800

    Fixed loading async

--- HackUCSC2015/HackUCSC2015/ViewController.swift
+++ HackUCSC2015/HackUCSC2015/ViewController.swift
@@ -21,10 +21,10 @@ class ViewController: ResponsiveTextFieldViewController, UIPickerViewDelegate{
     
     override func viewDidLoad() {
         
-        var newClass = MyRequestController()
+        //var newClass = MyRequestController()
         var schools = []
         
-        newClass.get() { (json: JSON) -> () in
+        get() { (json: JSON) -> () in
             
             for (key: String, subJson: JSON) in json {
                 
@@ -47,11 +47,11 @@ class ViewController: ResponsiveTextFieldViewController, UIPickerViewDelegate{
     
     @IBAction func onSendDataPress(sender: AnyObject) {
         
-        var webClass = MyRequestController();
+        //var webClass = MyRequestController();
         //sharedData(
         let json = JSON(cData())
         //println(jsooon)
-        webClass.sendRequest(json)
+        sendRequest(json)
         
     }
     
@@ -81,4 +81,119 @@ class ViewController: ResponsiveTextFieldViewController, UIPickerViewDelegate{
     func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String!{
         return contents[row]
     }
+    
+    
+    func get(completedFunction : (json: JSON) ->()) {
+
+        let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        /* Create session, and optionally set a NSURLSessionDelegate. */
+        let session = NSURLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        
+        /* Create the Request:
+        My API (2) (POST http://oneillseaodyssey.org/testing.php)
+        */
+        
+        var URL = NSURL(string: "http://oneillseaodyssey.org/applications.php")
+        let request = NSMutableURLRequest(URL: URL!)
+        request.HTTPMethod = "GET"
+        var myJson = JSON([1,2])
+        
+        /* Start a new Task */
+        let task = session.dataTaskWithRequest(request, completionHandler: { (data : NSData!, response : NSURLResponse!, error : NSError!) -> Void in
+            if (error == nil) {
+                // Success
+                let statusCode = (response as NSHTTPURLResponse).statusCode
+                let json = JSON(data: data)
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    
+                    for (key: String, subJson: JSON) in json {
+                        
+                        self.contents.append(subJson["school"].stringValue)
+                        self.dict[subJson["school"].stringValue] = subJson["id"].stringValue
+                        
+                    }
+                    if(self.schoolPicker != nil) {
+                        self.schoolPicker.reloadAllComponents()
+                    }
+                })
+                
+                
+                
+            }
+            else {
+                println("URL Session Task Failed: %@", error.localizedDescription);
+            }
+        })
+        
+        task.resume()
+    }
+    
+    func sendRequest(params : JSON) {
+
+        let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        /* Create session, and optionally set a NSURLSessionDelegate. */
+        let session = NSURLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        
+        /* Create the Request:
+        My API (2) (POST http://oneillseaodyssey.org/testing.php)
+        */
+        
+        var URL = NSURL(string: "http://oneillseaodyssey.org/parse.php")
+        let request = NSMutableURLRequest(URL: URL!)
+        request.HTTPMethod = "POST"
+        
+        
+        var foo = params.rawString(options:nil)
+        if let string : String = foo {
+
+            let bodyParameters = [
+                "data" : string
+            ]
+
+            println(string)
+            
+            let bodyString = stringFromQueryParameters(bodyParameters)
+            println ("bodyString:", bodyString);
+            request.HTTPBody = bodyString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+            
+            /* Start a new Task */
+            let task = session.dataTaskWithRequest(request, completionHandler: { (data : NSData!, response : NSURLResponse!, error : NSError!) -> Void in
+                if (error == nil) {
+                    // Success
+                    let statusCode = (response as NSHTTPURLResponse).statusCode
+                    println("URL Session Task Succeeded: HTTP \(statusCode)")
+                    println("returned data, should be identical:")
+                    var results = (NSString(data: data, encoding: NSUTF8StringEncoding)) as String
+                    results = results.stringByReplacingOccurrencesOfString("\\", withString: "")
+                    println(results)
+                }
+                else {
+                    // Failure
+                    println("URL Session Task Failed: %@", error.localizedDescription);
+                }
+            })
+            task.resume()
+        }
+    }
+    
+    func stringFromQueryParameters(queryParameters : Dictionary<String, String>) -> String {
+        var parts: [String] = []
+        for (name, value) in queryParameters {
+            var part = NSString(format: "%@=%@",
+                name.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!,
+                value.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)
+            parts.append(part)
+        }
+        return "&".join(parts)
+    }
+
+    func NSURLByAppendingQueryParameters(URL : NSURL!, queryParameters : Dictionary<String, String>) -> NSURL {
+        let URLString : NSString = NSString(format: "%@?%@", URL.absoluteString!, stringFromQueryParameters(queryParameters))
+        return NSURL(string: URLString)!
+    }
+
+    
 }
